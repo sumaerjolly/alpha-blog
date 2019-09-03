50fe02e4df2fa36f12a5ee2ee9e094a51e333add
Sumaer Jolly <sumaerjolly@gmail.com>
2019-09-03 17:38:06 +0530

    Extract redundnacy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+    
+    before_action :set_article , only: [:edit, :update, :show, :destroy]
+
     def new
         @article = Article.new
     end 
@@ -18,15 +21,12 @@ class ArticlesController < ApplicationController
     end 
 
     def show 
-        @article = Article.find(params[:id])
     end 
 
     def edit 
-        @article = Article.find(params[:id])
     end 
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Your article was susccessfully updated"
             redirect_to article_path(@article)
@@ -36,7 +36,6 @@ class ArticlesController < ApplicationController
     end 
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy 
         flash[:notice] = "Article was successfully deleted"
         redirect_to articles_path
@@ -44,6 +43,12 @@ class ArticlesController < ApplicationController
 
 
     private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end 
+
+
     def article_params
         params.require(:article).permit(:title, :description)
     end 
