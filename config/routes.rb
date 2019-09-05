a27a38352876373aee24ebf68e751beb03bf48cd
Sumaer Jolly <sumaerjolly@gmail.com>
2019-09-05 16:02:14 +0530

    sign up users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   
+  get 'users/new'
   root 'pages#home'
   get '/about', to: 'pages#about'
+  get 'signup', to: 'users#new'
   resources :articles
+  resources :users, except: [:new]
 
 end
